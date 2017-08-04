b1a17a174f6877bc3d2905fa48cef25c8c33f64f
dongxiangtao <allcates@qq.com>
2017-08-04 16:14:21 +0800

    commit

---
+++ YbkManage/App/AppConfig.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace YbkManage.App
+{
+    /// <summary>
+    /// app 配置的类（存放一些配置的属性或常量）
+    /// </summary>
+    public class AppConfig
+    {
+        public static string APP_ID = "1004";
+        public static string APP_KEY = "v5appkey_xf_865fn$xa";
+
+		public static string EncodeKey = "en32xamarindses81be0dc39232-mtfs";
+
+		/// <summary>
+		/// 用户资料
+		/// </summary>
+		public const string SP_USERINFO = "sp_userInfo";
+
+		/// <summary>
+		/// 上次登录账号
+		/// </summary>
+		public const string SP_LAST_LOGIN_ACCOUNT = "sp_last_loginaccount";
+
+		/// <summary>
+		/// 季度
+		/// </summary>
+		public const string SP_QUARTER = "sp_quarterInfo";
+
+        /// <summary>
+		/// 是否是开发环境 应用开发时，改为true; 应用发布时，改为false;
+		/// </summary>
+		public static bool API_IS_DEVELOPMENT = true;
+
+        /// <summary>
+        /// 开发环境平台接口host
+        /// </summary>
+        public static string API_DEVELOPMENT_HOST = "http://class.i.xdf.cn/";
+
+        /// <summary>
+        /// 正式环境平台接口host
+        /// </summary>
+        public static string API_PRODUCTION_HOST = "http://test.class.i.xdf.cn/";
+
+		/// <summary>
+		/// 接口的hostURL
+		/// </summary>
+		/// <returns>The get host.</returns>
+		public static string API_Get_HOST()
+        {
+            return API_IS_DEVELOPMENT ? API_DEVELOPMENT_HOST : API_PRODUCTION_HOST;
+		}
+
+		/// <summary>
+		/// 报表首页接口
+		/// </summary>
+		public static string API_INDEX_REPORT = "http://class.i.xdf.cn/Common/Index/";
+
+		/// <summary>
+		/// 报表模块所需接口
+		/// </summary>
+		public static string API_INDEX_REPORT2 = "http://xuban.xdf.cn/renew/index/";
+
+		/// <summary>
+		/// 教师管理接口
+		/// </summary>
+		public static string API_TEACHER_MANAGE = "http://member.i.xdf.cn/Teacher/Index";
+
+		/// <summary>
+		/// 用户的接口
+		/// </summary>
+		public static string API_USER_INDEX = "http://member.i.xdf.cn/User/Index";
+
+		/// <summary>
+		/// 用户信息
+		/// </summary>
+		public static string API_TEACHER_INFO = "http://ims.xdf.cn/imserver/message/getinfobyids";
+
+		/// <summary>
+		/// 课程信息
+		/// </summary>
+		public static string API_CLASS_INFO = "http://i.xdf.cn/upoc/Class/";
+
+        /// <summary>
+        /// 教学主管相关接口
+        /// </summary>
+        public static string API_ASSISTANT = "http://member.i.xdf.cn/Assistant/Index";
+
+    }
+}
--- YbkManage/Resources/Resource.designer.cs
+++ YbkManage/Resources/Resource.designer.cs
@@ -1579,88 +1579,121 @@ namespace YbkManage
 			public const int button_globaldialog_status = 2130837589;
 			
 			// aapt resource value: 0x7f020056
-			public const int class_1 = 2130837590;
+			public const int dialog_bg = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int class_2 = 2130837591;
+			public const int edittext_account = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int class_3 = 2130837592;
+			public const int edittext_bg = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int dialog_bg = 2130837593;
+			public const int edittext_clear = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int edittext_account = 2130837594;
+			public const int edittext_password = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int edittext_bg = 2130837595;
+			public const int ic_dili = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int edittext_clear = 2130837596;
+			public const int ic_huaxue = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int edittext_password = 2130837597;
+			public const int ic_lishi = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int icn_selected = 2130837598;
+			public const int ic_lizong = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int icn_toast_error = 2130837599;
+			public const int ic_more = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int icn_toast_smile = 2130837600;
+			public const int ic_shengwu = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int icn_toast_success = 2130837601;
+			public const int ic_shuxue = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int icn_toast_warning = 2130837602;
+			public const int ic_wenzong = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int icon_eidt = 2130837603;
+			public const int ic_wuli = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int login_logo = 2130837604;
+			public const int ic_yingyu = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int menu_index = 2130837605;
+			public const int ic_youcai = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int menu_index_on = 2130837606;
+			public const int ic_yuwen = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int menu_mine = 2130837607;
+			public const int ic_zhengzhi = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int menu_mine_on = 2130837608;
+			public const int ic_zhuanxiang = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int menu_teach = 2130837609;
+			public const int icn_selected = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int menu_teach_on = 2130837610;
-			
-			// aapt resource value: 0x7f020071
-			public const int notification_template_icon_bg = 2130837617;
+			public const int icn_toast_error = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int ripple_bg = 2130837611;
+			public const int icn_toast_smile = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int splash_logo = 2130837612;
+			public const int icn_toast_success = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int textview_bg = 2130837613;
+			public const int icn_toast_warning = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int textview_bg_on = 2130837614;
+			public const int icon_eidt = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int textview_circle_bg = 2130837615;
+			public const int login_logo = 2130837615;
 			
 			// aapt resource value: 0x7f020070
-			public const int toast_bg = 2130837616;
+			public const int menu_index = 2130837616;
+			
+			// aapt resource value: 0x7f020071
+			public const int menu_index_on = 2130837617;
+			
+			// aapt resource value: 0x7f020072
+			public const int menu_mine = 2130837618;
+			
+			// aapt resource value: 0x7f020073
+			public const int menu_mine_on = 2130837619;
+			
+			// aapt resource value: 0x7f020074
+			public const int menu_teach = 2130837620;
+			
+			// aapt resource value: 0x7f020075
+			public const int menu_teach_on = 2130837621;
+			
+			// aapt resource value: 0x7f02007c
+			public const int notification_template_icon_bg = 2130837628;
+			
+			// aapt resource value: 0x7f020076
+			public const int ripple_bg = 2130837622;
+			
+			// aapt resource value: 0x7f020077
+			public const int splash_logo = 2130837623;
+			
+			// aapt resource value: 0x7f020078
+			public const int textview_bg = 2130837624;
+			
+			// aapt resource value: 0x7f020079
+			public const int textview_bg_on = 2130837625;
+			
+			// aapt resource value: 0x7f02007a
+			public const int textview_circle_bg = 2130837626;
+			
+			// aapt resource value: 0x7f02007b
+			public const int toast_bg = 2130837627;
 			
 			static Drawable()
 			{
