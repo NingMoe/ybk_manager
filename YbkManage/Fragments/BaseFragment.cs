9867f3503e59dd006cd4576c24d8deb555dde75c
June <zanjli2012@gmail.com>
2017-08-11 19:03:23 +0800

    fragments,1

--- YbkManage/Fragments/BaseFragment.cs
+++ YbkManage/Fragments/BaseFragment.cs
@@ -7,26 +7,29 @@ using YbkManage.Activities;
 using YbkManage.App;
 using YbkManage.Models;
 
+using Com.Umeng.Analytics;
+using DataService;
+
 namespace YbkManage.Fragments
 {
 	/// <summary>
 	/// 所有Fragment的基类
 	/// </summary>
 	public class BaseFragment : Fragment
-    {
-        /// <summary>
-        /// 当前所在的Activity
-        /// </summary>
-        public AppActivity CurrActivity;
+	{
+		/// <summary>
+		/// 当前所在的Activity
+		/// </summary>
+		public AppActivity CurrActivity;
 
-        /// <summary>
-        /// 当前登录信息
-        /// </summary>
+		/// <summary>
+		/// 当前登录信息
+		/// </summary>
 		protected LoginUserInfoEntity CurrUserInfo;
 
-        public override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+		public override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 
 			CurrActivity = (AppActivity)this.Activity;
 			string userinfoStr = (string)SharedPreferencesUtil.GetParam(CurrActivity, AppConfig.SP_USERINFO, "");
@@ -34,11 +37,28 @@ namespace YbkManage.Fragments
 			{
 				CurrUserInfo = DataService.Helper.FromJsonTo<LoginUserInfoEntity>(userinfoStr);
 			}
-        }
+		}
 
-        public override void OnResume()
-        {
-            base.OnResume();
-        }
+		#region 友盟统计
+		public override void OnResume()
+		{
+			base.OnResume();
+			if (Config.UMengIsOpen)
+			{
+				MobclickAgent.OnPageStart(CurrActivity.Title);
+				MobclickAgent.OnResume(CurrActivity.BaseContext);
+			}
+		}
+
+		public override void OnPause()
+		{
+			base.OnPause();
+			if (Config.UMengIsOpen)
+			{
+				MobclickAgent.OnPageEnd(CurrActivity.Title);
+				MobclickAgent.OnPause(CurrActivity.BaseContext);
+			}
+		}
+		#endregion
     }
 }
